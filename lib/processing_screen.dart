579e708d39bc6e8f8a8904dbbf3aaa855c2b9241
Mellak <youness.mellak.96@gmail.com>
2019-12-07 10:43:27 +0100

    Flutter application to implement IBM visual recognition on YOUR app.

---
+++ lib/processing_screen.dart
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2019 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:ibm_visual_recog_img_file/IBMVisualRecognition.dart';
+import 'package:ibm_visual_recog_img_file/connection.dart';
+
+class ProcessingImageScreen extends StatefulWidget {
+  final String imagePath;
+  String WatsonOutput;
+  ProcessingImageScreen({this.imagePath});
+
+  @override
+  _ProcessingImageScreenState createState() => _ProcessingImageScreenState();
+}
+
+class _ProcessingImageScreenState extends State<ProcessingImageScreen> {
+  String _text;
+  StreamController controller = StreamController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preview'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+                flex: 2,
+                child: Image.file(File(widget.imagePath), fit: BoxFit.cover)),
+            SizedBox(height: 30.0),
+            StreamBuilder(
+                /*stream: StreamVR(File(widget.imagePath),
+                    "dMmIylYjTTp-4DTK6qw5mx_t3LAcJIuPYZsOrZWdhuvz"),*/
+                stream: StreamMyClassifier(
+                    File(widget.imagePath),
+                    "dMmIylYjTTp-4DTK6qw5mx_t3LAcJIuPYZsOrZWdhuvz",
+                    "DefaultCustomModel_1093175042"),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  _text = snapshot.data;
+                  return Text(
+                      _text != null ? '${_text}' : 'Watson Response Here',
+                      style: Theme.of(context).textTheme.display1);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<ByteData> getBytesFromFile() async {
+    Uint8List bytes = File(widget.imagePath).readAsBytesSync() as Uint8List;
+    return ByteData.view(bytes.buffer);
+  }
+}
